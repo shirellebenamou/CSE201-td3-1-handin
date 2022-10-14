c9b0ef05869b8ac4312b19bf80d21ad9017dfd67
shirellebenamou <115411008+shirellebenamou@users.noreply.github.com>
2022-10-14 14:35:51 +0200

    7

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char* argv[])
 
   {
 
-    std::cout << "A better, new message" << std::endl;
+    std::cout << "A better, better new message" << std::endl;
 
 }
   // END OF THE CUSTOM CODE SECTION
